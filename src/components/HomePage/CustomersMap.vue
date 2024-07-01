8c6e1cc3b48fea5d47dceebcdef66488e3038fca
jingjingh17 <jingjingh71@gmail.com>
2024-07-01 15:32:56 +0800

    更新/home页。新增：客户分布统计图，销售数据展示

--- src/components/HomePage/CustomersMap.vue
+++ src/components/HomePage/CustomersMap.vue
@@ -1,8 +1,83 @@
 <template>
-    <p>客户分布</p>
+    <div ref="chartContainer" style="width: 100%; height: 600px;"></div>
 </template>
 
 <script>
-</script>
+import * as echarts from 'echarts';
 
-<style scoped></style>
+export default {
+    name: 'CustomersMap',
+    data() {
+        return {
+            chartInstance: null,
+        };
+    },
+    async mounted() {
+        // 加载地图JSON文件
+        const mapJson = require('../../assets/maps/china.json');
+        echarts.registerMap('China', mapJson);
+
+        // 初始化Echarts实例
+        this.chartInstance = echarts.init(this.$refs.chartContainer);
+
+        // 配置项和数据
+        const option = {
+            title: {
+                text: '客户分布统计图',
+            },
+            tooltip: {
+                trigger: 'item',
+            },
+            visualMap: {
+                min: 0,
+                max: 100,
+                left: 'left',
+                top: 'bottom',
+                text: ['高', '低'],
+                calculable: true,
+                inRange: {
+                    color: ['#e0f2f1', '#006064'],
+                },
+            },
+            series: [
+                {
+                    type: 'map',
+                    mapType: 'China',
+                    // 控制地图是否可以缩放
+                    roam: true,
+                    zoom: 1.2,
+                    label: {
+                        show: true,
+                    },
+                    emphasis: {
+                        label: {
+                            show: true,
+                            fontSize: 14,
+                        },
+                    },
+                    data: [
+                        { name: '北京市', value: 49 },
+                        { name: '上海市', value: 95 },
+                        { name: '甘肃省', value: 999 },
+                        // 更多数据...
+                    ],
+                },
+            ],
+        };
+
+        // 显示图表
+        this.chartInstance.setOption(option);
+    },
+    methods: {
+        async loadMapJson(path) {
+            const response = await fetch(path);
+            return await response.json();
+        },
+    },
+    beforeDestroy() {
+        if (this.chartInstance) {
+            this.chartInstance.dispose();
+        }
+    },
+};
+</script>
--- src/components/HomePage/SuppliersMap.vue
+++ src/components/HomePage/SuppliersMap.vue
@@ -1,10 +1,104 @@
 <template>
-    <p>供应商分布</p>
-    </template>
-    
-    <script>
-    </script>
-    
-    <style scoped>
-    
-    </style>
+    <div ref="chartContainer" :style="{ width: '100%', height: '600px' }"></div>
+</template>
+
+<script>
+import * as echarts from 'echarts';
+
+export default {
+    name: 'SalesComparisonChart',
+    data() {
+        return {
+            chartInstance: null,
+        };
+    },
+    async mounted() {
+        this.chartInstance = echarts.init(this.$refs.chartContainer);
+        this.renderChart();
+    },
+    methods: {
+        renderChart() {
+            const option = {
+                title: {
+                    text: '销售数据',
+                    left: 'left',
+                },
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'cross',
+                    },
+                },
+                legend: {
+                    data: ['销售额', '销售量'],
+                    top: 10,
+                },
+                xAxis: {
+                    type: 'category',
+                    boundaryGap: false,
+                    data: ['2019 Q1', '2019 Q2', '2019 Q3', '2019 Q4', '2020 Q1', '2020 Q2', '2020 Q3', '2020 Q4'],
+                },
+                yAxis: [
+                    {
+                        type: 'value',
+                        name: '销售额（万元）',
+                        position: 'left',
+                        axisLine: {
+                            lineStyle: {
+                                color: '#dd4444',
+                            },
+                        },
+                        axisLabel: {
+                            formatter: '{value} 万'
+                        }
+                    },
+                    {
+                        type: 'value',
+                        name: '销售量（件）',
+                        position: 'right',
+                        offset: 60,
+                        axisLine: {
+                            lineStyle: {
+                                color: '#44dd44',
+                            },
+                        },
+                        axisLabel: {
+                            formatter: '{value}'
+                        }
+                    },
+                ],
+                // 配置图表的左右间距等
+                grid:{
+                    right: '15%',
+                    left: '5%',
+                },
+                series: [
+                    {
+                        name: '销售额',
+                        type: 'line',
+                        
+                        smooth: true,
+                        yAxisIndex: 0,
+                        data: [20, 25, 30, 35, 40, 45, 50, 55],
+                    },
+                    {
+                        name: '销售量',
+                        type: 'bar',
+                        yAxisIndex: 1,
+                        data: [500, 600, 700, 800, 900, 1000, 1100, 1200],
+                        // 柱状图宽度
+                        barWidth:40,
+                    },
+                ],
+            };
+
+            this.chartInstance.setOption(option);
+        },
+        beforeDestroy() {
+            if (this.chartInstance) {
+                this.chartInstance.dispose();
+            }
+        },
+    },
+};
+</script>
